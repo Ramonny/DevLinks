c6a47a7ecdc268bd6cb329849f5bed81995acf5a
Ramonny <ramonny.joao@gmail.com>
2024-04-15 16:54:07 -0300

    removed description in javascript, stylecss stylesheet changes

--- script.js
+++ script.js
@@ -2,13 +2,12 @@ function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
 
+  // pegar a tag img
   const img = document.querySelector("#profile img")
+
+  // substituir a imagem
   if (html.classList.contains("light")) {
     img.setAttribute("src", "./assets/avatar-light.png")
-    img.setAttribute(
-      "alt",
-      "Foto de Ramonny, de óculos escuro, olhando para frente"
-    )
   } else {
     img.setAttribute("src", "./assets/avatar.png")
   }
